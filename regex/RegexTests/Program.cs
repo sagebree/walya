97dde54a9c67c309c08043aff945bee0986c9055
Sam <nay_sami@yahoo.com>
2014-02-03 14:12:24 -0800

    updated program.cs

--- regex/RegexTests/Program.cs
+++ regex/RegexTests/Program.cs
@@ -12,13 +12,12 @@ namespace RegexTests
         static void Main(string[] args)
         {
             string input = @"Branch ship.client.main.w5m2 2011/10/25 01:09:28 'Created by CreateBranch script for REDMOND\BTSA010 '";
-            string pattern = "Branch (.+?) (.*)";
+            string pattern = "Branch (.+?) .*";
             Match match = Regex.Match(input, pattern);
-            //while (match.Success)
+            if (match.Success && match.Groups.Count == 2)
             {
-                Console.WriteLine( match.Groups[0].Value);
+                Console.WriteLine(match.Groups[0].Value);
                 Console.WriteLine(match.Groups[1].Value);
-                Console.WriteLine(match.Groups[2].Value);
                 Console.WriteLine(match.Groups.Count);
             }
             Console.ReadLine();
